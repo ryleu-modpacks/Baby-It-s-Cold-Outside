d8dabcff93ffa87833eb3e4d0d9f5cf957d89341
VigilanteReal <aspapageorge@outlook.com>
2024-12-26 23:26:57 -0500

    - moving recipes/figuring out kubejs bs

---
+++ kubejs/server_scripts/recipe.js
@@ -0,0 +1,14 @@
+ServerEvents.recipes(event => {
+		
+    event.remove({ output: 'fromanotherworld:flamethrower_mk_1' })
+    
+    event.remove({ output: 'repaired_disc_eleven:boom' })
+    
+    
+    
+    
+    
+    
+    console.log('Heyo! The recipe event has been fired!')
+    
+})
