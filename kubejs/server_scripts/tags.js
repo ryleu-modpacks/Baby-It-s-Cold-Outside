2aa488c83dab7915d6c1aad85b110bb934b2d896
ryleu <69326171+ryleu@users.noreply.github.com>
2025-01-05 02:38:53 -0500

    change the indentation size

--- kubejs/server_scripts/tags.js
+++ kubejs/server_scripts/tags.js
@@ -1,10 +1,10 @@
 ServerEvents.tags('item', event => {
-    event.add('c:nuggets/copper', 'create:copper_nugget')
-    event.add('c:nuggets/copper', 'consistency_plus:copper_nugget')
-    event.add('c:nuggets/copper', 'progression_reborn:copper_nugget')
+  event.add('c:nuggets/copper', 'create:copper_nugget')
+  event.add('c:nuggets/copper', 'consistency_plus:copper_nugget')
+  event.add('c:nuggets/copper', 'progression_reborn:copper_nugget')
 
-    event.add('c:nuggets/diamond', 'progression_reborn:diamond_nugget')
-    event.add('c:nuggets/diamond', 'create_dd:diamond_shard')
+  event.add('c:nuggets/diamond', 'progression_reborn:diamond_nugget')
+  event.add('c:nuggets/diamond', 'create_dd:diamond_shard')
 
-    console.log('modified tags for modpack');
+  console.log('modified tags for modpack');
 })
